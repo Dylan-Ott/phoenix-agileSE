3687303bf202b37e45aad43bb468c9135d6ce997
Dylan-Ott <dylan.ott.11@live.com>
2023-10-17 00:37:29 -0400

    Initial Commit
    Sets up the initial project as an Angular project. Components for the UI prototype have been generated (but not worked on!) and the project builds and runs

---
+++ PhoenixEcomm/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { RegistrationPageComponent } from './registration-page/registration-page.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LandingPageComponent,
+    LoginPageComponent,
+    RegistrationPageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
